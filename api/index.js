7cb40655f65d4b79edc46ea904447656e9e1afa7
afridhotullulu <lafridhotul@gmail.com>
2026-06-19 11:14:36 +0700

    fix session passport order

--- api/index.js
+++ api/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const session = require('express-session');
 const passport = require('passport');
 
 const app = express();
 
+// STEP 3 - MIDDLEWARE
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// STEP 4 - SERIALIZE
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+// ROUTE LOGIN
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 
 app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/auth/failed', session: false }),
+  passport.authenticate('google', { failureRedirect: '/auth/failed' }),
   (req, res) => {
     res.send('Login berhasil');
   }
