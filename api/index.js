c902c33a602883e26d72f72ada25b406b28cf406
afridhotullulu <lafridhotul@gmail.com>
2026-06-19 14:03:57 +0700

    add start script

--- api/index.js
+++ api/index.js
@@ -48,3 +48,8 @@ app.get('/', (req, res) => {
 });
 
 module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
