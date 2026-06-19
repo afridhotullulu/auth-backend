cfdff355044b4ddc653aab10234c3d4be35fcea0
afridhotullulu <lafridhotul@gmail.com>
2026-06-19 13:46:24 +0700

    fix vercel backend

--- api/index.js
+++ api/index.js
@@ -1,50 +1,5 @@
 const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const jwt = require('jsonwebtoken');
-
 const app = express();
-
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL
-}, (accessToken, refreshToken, profile, done) => {
-  return done(null, profile);
-}));
-
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/auth/failed', session: false }),
-  (req, res) => {
-    const token = jwt.sign(req.user, process.env.JWT_SECRET, { expiresIn: '7d' });
-
-  res.redirect(`${process.env.FRONTEND_URL}/dashboard?token=${token}`)
-  }
-);
-
-app.get('/auth/failed', (req, res) => {
-  res.send('Login gagal');
-});
 
 app.get('/', (req, res) => {
   res.send('Backend OK');
